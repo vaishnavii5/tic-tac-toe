d2a48cbbbee4f0496e568b0f240680658201a8a4
veelocity <vaishnavit.006@gmail.com>
2024-07-09 13:41:45 +0530

    commit

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './TicTacToe.css'
 import circle_icon from "../Assets/circle.png"
 import cross_icon from "../Assets/cross.png"
@@ -9,6 +9,7 @@ const TicTacToe = () => {
 
     let [count, setCount] = useState(0);
     let [lock, setLock] = useState(false);
+    let titleRef = useRef(null);
 
     const toggle = (e, num) => {
         if(lock) {
@@ -24,11 +25,53 @@ const TicTacToe = () => {
             data[num] = "o";
             setCount(++count);
         }
+        checkWin();
+    }
+
+    const checkWin = () => {
+        if(data[0]===data[1] && data[1]===data[2] && data[2] !== "") {
+            won(data[2]);
+        }
+        else if(data[3]===data[4] && data[4]===data[5] && data[5] !== "") {
+            won(data[5]);
+        }
+        else if(data[6]===data[7] && data[7]===data[8] && data[8] !== "") {
+            won(data[8]);
+        }
+        else if(data[0]===data[3] && data[3]===data[6] && data[6] !== "") {
+            won(data[6]);
+        }
+        else if(data[1]===data[4] && data[4]===data[7] && data[7] !== "") {
+            won(data[7]);
+        }
+        else if(data[2]===data[5] && data[5]===data[8] && data[8] !== "") {
+            won(data[8]);
+        }
+        else if(data[0]===data[4] && data[4]===data[8] && data[8] !== "") {
+            won(data[8]);
+        }
+        else if(data[0]===data[1] && data[1]===data[2] && data[2] !== "") {
+            won(data[2]);
+        }
+        else if(data[2]===data[4] && data[4]===data[6] && data[6] !== "") {
+            won(data[6]);
+        }
+    }
+
+    const won = (winner) => {
+        setLock(true);
+        if(winner === "x"){
+                titleRef.current.innerHTML = `PLAYER 1 WON!`;
+        }
+        else {
+            titleRef.current.innerHTML = `PLAYER 2 WON!`;
+        }
     }
 
   return (
     <div className='container'>
         <h1 className="title">TIC-TAC-TOE</h1>
+        <h2 className='subtitle' ref={titleRef}>Game In Progress</h2>
         <div className='board'>
             <div className="row1">
                 <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
