cb68a9a0ba3cb7535a428f0930695e811a5b3553
veelocity <vaishnavit.006@gmail.com>
2024-07-09 13:24:30 +0530

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import Board from "./Board";
-import Info from "./Info";
-import "./App.css";
-import { useState } from "react";
+
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
 
 function App() {
-    const [reset, useReset] = useState(false);
-    const [winner, setWinner] = useState("");
-
-    const resetBoard = () => {
-        setReset(true);
-    };
-
-    return (
-        <div className="App">
-            <div className={`winner ${
-                winner !== "" ? "" : "shrink"
-            }`}>
-                <div className="winner-text">{winner}</div>
-                <button onClick={() => resetBoard()}>RESET</button>
-            </div>
-            <Board
-                reset = {reset}
-                setReset = {setReset}
-                winner = {winner}
-                setWinner = {setWinner}
-            />
-        </div>
-    );
-};
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
 
 export default App;
